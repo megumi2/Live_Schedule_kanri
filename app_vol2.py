5a26e16dec11fe121c640889b363a6fbfce0b749
megumi2 <megumi.deai0820@gmail.com>
2023-07-27 12:14:35 +0900

    2nd commit

--- app_vol2.py
+++ app_vol2.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[77]:
-
-
 import requests
 import re
 from bs4 import BeautifulSoup
@@ -11,113 +5,69 @@ import time
 import pandas as pd
 
 
-# In[78]:
-
-
-oneman_url = "http://buglug.jp/live/live-oneman/"
+#URLから情報を取得
 event_url = "http://buglug.jp/live/live-event/"
-
-
-# In[79]:
-
-
-oneman = requests.get(oneman_url)
 event = requests.get(event_url)
 time.sleep(3)
 
-
-# In[80]:
-
-
-oneman_bs = BeautifulSoup(oneman.text, 'html.parser')
 event_bs = BeautifulSoup(event.text, 'html.parser')
-
-
-# In[81]:
-
 
 #スケジュールのリンク先を取得
 link = event_bs.select(".live-list > ul > a ")
 event_link = []
 for i in link:
-    try:
-        i = i.get("href")
-        event_link.append(i)
-    except:
-        pass
-print(event_link)
+    i = i.get("href")
+    event_link.append(i)
 
 
-# In[82]:
 
-
-place = []
-etitle = []
-place_list = []
-ti_list = []
-place_2 = []
-place_3 = []
+title_list = []
+place_day_list = []
 date_list = []
-list_tmp = []
+open_list = []
+place_list = []
 
-
-# In[83]:
-
-
-for i in range(0, len(event_link)):
-    place_link = requests.get(event_link[i])    
+for i in event_link:
+    place_link = requests.get(i)    
     time.sleep(0.5)
     event_bs = BeautifulSoup(place_link.text, 'html.parser')
-    place = event_bs.select(".live-place")
-    etitle = event_bs.select("h3.title")
-    openlive =  event_bs.select("p.live-open")
-    for j in place:
-        place_tmp = []
-        place_text = j.get_text().strip()
-        place_tmp.append(place_text)
-        place_list.append(place_tmp)
-    #ライブタイトルを埋めるために必要な処理        
-    for k in etitle:
-        etitle = k.get_text()
-        for j in place:
-            place_tmp_2 = []
-            place_text_2 = re.split('\(?[日月火水木金土祝]\)', j.get_text())
-            place_tmp_2.append(place_text_2)
-            for t in range(len(place_tmp_2[0])-1):
-                ti_list.append(etitle)
-#曜日を分ける
-for sublist in place_list:
-    for element in sublist:
-        list_tmp.append(re.split('\(?[日月火水木金土祝]\)', element))
-#日程を日程リストに、ライブ会場をライブ会場リストに格納させる
-for sublist in list_tmp:
+    place_data_list = event_bs.select(".live-place")
+    openlive_data_list =  event_bs.select("p.live-open")
+    title_data_list = event_bs.select("h3.title")
+    #曜日ごとに分割
+    for j in place_data_list:
+        place_day_tmp = []
+        place_day_text = re.split('\(?[日月火水木金土祝]\)', j.get_text().replace('\r\n', '').replace(' ', '').replace('\n\n', '').replace('\n', '').replace('\u25b6', ' '))
+        place_day_list.append(place_day_text)
+    for i in title_data_list:
+        title_text = i.get_text()
+        if len(place_day_text) > 2:
+                for m in range(len(place_day_text)-1):
+                    title_list.append(title_text)
+        else:
+            for k in range(len(place_data_list)):
+                title_list.append(title_text)
+
+#会場と日程を分けて日程リスト、会場リストにそれぞれ格納
+
+for sublist in place_day_list:
     for element in sublist:
         if not re.search('\d{1,2}日' ,element):
             if len(sublist) > 2:
                 for j in range(len(sublist)-1):
-                    place_3.append(element)
+                    place_list.append(element)
             else:
-                place_3.append(element)           
+                place_list.append(element)           
         else:
             date_list.append(element)
 
-#タイトルを取得するぞい
-
-
-#event_dict = {'event_date': date_list, 'event_place': place_3, 'event_title': ti_list}
-#event_df = pd.DataFrame(event_dict)
-#event_df
-
-
-# In[84]:
-
-
-event_dict = {'event_date': date_list, 'event_place': place_3, 'event_title': ti_list}
+#表にまとめてcsvに出力
+event_dict = {'event_date': date_list, 'event_place': place_list, 'live_title': title_list}
 event_df = pd.DataFrame(event_dict)
-event_df
+event_df.to_csv('test.csv' ,encoding='cp932' ,index=False, errors='backslashreplace')
 
 
-# In[ ]:
+
 
 
 
